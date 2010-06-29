eaf12c0629c212f6e9d64802659d5c7ba67fdb75
silverkey <noncoding@gmail.com>
2010-06-29 12:51:59 +0200

    adding script to change X in N

---
+++ change_X_to_N.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Bio::SeqIO;
+
+my $folder = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_2/'; # name of the folder containing the fasta
+my $fasta = 'SEQs.fa.screen'; # name of the fasta to change
+
+# go into the directory
+chdir($folder);
+
+my $in = Bio::SeqIO->new(-file => $fasta,
+                         -format => 'fasta');
+
+my $out = Bio::SeqIO->new(-file => ">$fasta\.N",
+                          -format => 'fasta');
+
+while(my $seq = $in->next_seq) {
+  my $string = $seq->seq;
+	$string =~ s/X/N/g;
+	$seq->seq($string);
+	$out->write_seq($seq);
+}
+
+# change also the quality
+system("cp $fasta\.qual $fasta\.N\.qual");
