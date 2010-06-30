37e962a92ee8a04c67c54806ba1994336483cbbc
silverkey <noncoding@gmail.com>
2010-06-30 10:18:33 +0200

    minor revisions

--- parse_portrait.pl
+++ parse_portrait.pl
@@ -10,13 +10,14 @@ chdir($folder);
 open(IN,$file);
 
 while(my $row = <IN>) {
+	# The next line function only if you save the html file from the firefox under mac
   next unless $row =~ /^\<Tr\>\<Td.bgcolor\=\#.+\>(.+)\<\/Td\>\<Td bgcolor\=\#.+align\=\"right\"\>(\d+\.\d+)\%\<\/Td\>\<Td.bgcolor\=\#.+.align\=\"right\"\>(\d+\.\d+)\%\<\/Td\>$/;
   my $id = $1;
   my $coding = 1 - ($2 / 100);
   my $noncoding = 1 - ($3 / 100);
   $id =~ s/ //g;
-	$coding =~ s/^(\d+\.\d\d\d\d)\d+$/$1/;
-	$noncoding =~ s/^(\d+\.\d\d\d\d)\d+$/$1/;
+  $coding =~ s/^(\d+\.\d\d\d\d)\d+$/$1/;
+  $noncoding =~ s/^(\d+\.\d\d\d\d)\d+$/$1/;
   my $found = 'N';
   $found = 'Y' if $noncoding <= $noncoding_cutoff;
   print "$id\t$coding\t$noncoding\t$found\n";
--- run_cap3.pl
+++ run_cap3.pl
@@ -4,11 +4,15 @@ use warnings;
 
 # SET UP THE VARIABLES CONTAINING THE NAMES OF THE FOLDERS AND THE LOCATIONS OF THE FILES
 my $cap3 = '/home/remo/Desktop/POLPO_EST/PROGRAMS/CAP3/cap3'; # cap3 executable
-my $fasta_dir = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_2/'; # directory containing the fasta and the quality files - HERE WILL BE SAVED THE RESULTS ALSO
-my $fasta = 'SEQs.fa.screen'; # name of the fasta file - THE QUALITY IS TO BE IN THE SAME DIR WITH EXACTLY THE SAME NAME OF THE FASTA PLUS THE .QUAL EXTENSION
+my $fasta_dir = '/media/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/3_DB/'; # directory containing the fasta and the quality files - HERE WILL BE SAVED THE RESULTS ALSO
+my $fasta = 'clone.fa'; # name of the fasta file - THE QUALITY IS TO BE IN THE SAME DIR WITH EXACTLY THE SAME NAME OF THE FASTA PLUS THE .QUAL EXTENSION
+my $res_dir = '/media/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/4_CAP3/'; # directory in which to run
 
-# go into directory in which to put results
+# go into directory with the fasta
 chdir($fasta_dir);
 
 # launch the analysis
 system("$cap3 $fasta \> cap.out");
+
+# mv the results to the results directory
+system("mv \*\.cap\.\* $res_dir");
