b40de420a10ac5478641e5a9bf27d17a0d259fe0
silverkey <noncoding@gmail.com>
2010-08-05 11:08:26 +0200

    minor revisions

--- add_seq_table_to_db.pl
+++ add_seq_table_to_db.pl
@@ -8,14 +8,25 @@ use Bio::SeqIO;
 
 use Data::Dumper;
 
-my $DB = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/3_DB/OCTO_EST_3'; # Name and path of the database
-my $FASTA = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/5_SEQTRIM/unique.trimmed.fa'; # Name and path of the fasta file
-my $QUAL =  '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/5_SEQTRIM/unique.trimmed.fa.qual'; # Name and path of the quality file
+my $DB = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/3_DB/OCTO_EST_TEST'; # Name and path of the database
+#my $FASTA = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/5_SEQTRIM/unique.trimmed.fa'; # Name and path of the fasta file
+#my $QUAL =  '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/5_SEQTRIM/mod_unique.trimmed.fa.qual'; # Name and path of the quality file
+#my $RES_DIR = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/';
+
+my $FASTA = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/7_PORTRAIT/PORTRAIT_EYE/OctEye_NR.seq.fasta'; # Name and path of the fasta file
+my $QUAL =  0; # Name and path of the quality file
+my $RES_DIR = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/';
+
 my $CREATE_TABLE = 1; # Create the table (1) or only add the rows (0)?
-my $RES_DIR = '/Volumes/PACKY/POLPO_EST/ALL_ESTs_ANALYSIS/TEST_3/6_PORTRAIT/';
+my $ID_IN_FA = 'name'; # The field of the table to use as ID in the fasta to generate
+my $MINIMUM_LENGTH = 1; # The minimum length to put in the fasta to generate
 
 # THE TABLE WITH THE PHRED OUTPUT MUST TO BE CALLED clone !!!!
-my $TABNAME = 'unique_trimmed'; # Name of the table to populate
+# ALL THE OTHERS CAN BE CALLED AS YOU WANT....
+# my $TABNAME = 'EST'; # Name of the table to populate
+# THE TABLE WITH THE PHRED OUTPUT MUST TO BE CALLED clone !!!!
+# ALL THE OTHERS CAN BE CALLED AS YOU WANT....
+my $TABNAME = 'EYE';
 
 chdir($RES_DIR);
 
@@ -51,6 +62,7 @@ sub prepare_data {
     $DATA->{$seq->id}->{length} = $seq->length;
     $DATA->{$seq->id}->{desc} = $seq->desc;
   }
+  return unless $QUAL;
   my $qual = Bio::SeqIO->new(-file => $QUAL,
                               -format => 'qual');
   while(my $seq = $qual->next_seq()) {
@@ -63,12 +75,12 @@ sub insert_data {
   my $sth = $DBH->prepare_cached(qq{
     INSERT INTO $TABNAME
     (name,seq,qual,desc,length)
-    VALUES (?,?,?,?)
+    VALUES (?,?,?,?,?)
   });
 
   foreach my $name(keys %$DATA) {
     my $seq = $DATA->{$name}->{seq};
-    my $qual = $DATA->{$name}->{qual};
+    my $qual = $DATA->{$name}->{qual} || 'NA';
     my $desc = $DATA->{$name}->{desc};
     my $length = $DATA->{$name}->{length};
 
@@ -81,26 +93,26 @@ sub write_seqio {
                               -format => 'fasta');
 
   my $seqioq = Bio::SeqIO->new(-file => ">$TABNAME\.qual",
-                               -format => 'qual');
+                               -format => 'qual') if $QUAL;
 
-  my $sth = $DBH->prepare("SELECT * FROM $TABNAME WHERE length > 0");
+  my $sth = $DBH->prepare("SELECT * FROM $TABNAME WHERE length > $MINIMUM_LENGTH");
 
   $sth->execute;
 
   while(my $res = $sth->fetchrow_hashref) {
 
-    my $id = $res->{id};
+    my $id = $res->{$ID_IN_FA};
     my $seq = $res->{seq};
-    my $qual = $res->{qual};
-    $qual =~ s/\,/ /g;
+    my $qual = $res->{qual} if $QUAL;
+    $qual =~ s/\,/ /g if $QUAL;
 
     my $seqobj = Bio::Seq->new(-id => $id,
                                -seq => $seq);
 
     my $qualobj = Bio::Seq::PrimaryQual->new(-id => $id,
-                                             -qual => $qual);
+                                             -qual => $qual) if $QUAL;
 
     $seqio->write_seq($seqobj);
-    $seqioq->write_seq($qualobj);
+    $seqioq->write_seq($qualobj) if $QUAL;
   }
 }
